bd29677d6c287ce3afef1f8b62c6628f81036733
Astartling <2695928997@qq.com>
2019-03-26 20:33:18 +0800

    gh的第一次提交

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import styles from './index.scss';
 import React, { Component } from 'react';
-import { Menu, Icon } from 'antd';
+import { Menu, Icon, Carousel } from 'antd';
+
 
 class index extends Component {
     render() {
@@ -9,16 +10,51 @@ class index extends Component {
                 <div className={styles.home_header}>
                     <div className={styles.home_header_icon}></div>
                     <div className={styles.home_header_navigate}>
-                        <Menu  mode="horizontal">
-                            <Menu.Item key="mail"><Icon type="mail" />首页</Menu.Item>
-                            <Menu.Item key="app" ><Icon type="appstore" />社团公告</Menu.Item>
-                            <Menu.Item key="alipay"><a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a></Menu.Item>
+                        <Menu mode="horizontal">
+                            <Menu.Item key="mail">首页</Menu.Item>
+                            <Menu.Item key="notice" >社团公告</Menu.Item>
+                            <Menu.Item key="activity" >社团活动</Menu.Item>
+                            <Menu.Item key="prize" >社团奖</Menu.Item>
+                            <Menu.Item key="all" >全部社团</Menu.Item>
+                            <Menu.Item key="center" ><a href="https://ant.design" target="_blank" rel="noopener noreferrer">个人中心</a></Menu.Item>
+                            {/* <Menu.Item key="alipay" ><a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a></Menu.Item> */}
                         </Menu>
                     </div>
                 </div>
-                <div className={styles.home_content}></div>
+                <div className={styles.home_content}>
+                    <div className={styles.home_content_text}>
+                        <div className={styles.home_content_tag}>
+                            <a href="index.html">
+                                加入我们<br />
+                                JOIN US<br /></a>
+                            <hr />
+                        </div>
+                        <div className={styles.home_content_tagtwo}>
+                            <a href="index.html">了解我们</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.home_main_activity}>
+                    <div className={styles.home_main_topName}>
+                        社团公告
+                   </div>
+                    <div className={styles.home_main_topSubName}>
+                        Notice
+                   </div>
+                    <div className={styles.home_main_pictures}>
+                        <Carousel autoplay>
+                            <div><h3>1</h3></div>
+                            <div><h3>2</h3></div>
+                            <div><h3>3</h3></div>
+                            <div><h3>4</h3></div>
+                        </Carousel>,
+                        mountNode
+                  <img src="../../assets/headerbc.jpeg" alt="" />
+                    </div>
+                </div>
                 {/* <div className={styles.home_footer}>Footer</div> */}
             </div>
+
         )
     }
 }
