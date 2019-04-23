b0d4181c60ba7c7514cbac156fe3011f3835f00d
jsl <920024814@qq.com>
2019-04-23 23:16:38 +0800

    更新了一览表页面布局，增添了自定义组件，使得更方便的使用、统一控制导航栏，使用方式先引入后<Cswheader/>

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -7,19 +7,23 @@ import { Link } from 'dva/router';
 
 function IndexPage() {
   return (
-    <div className={styles.index}>
-      项目一览表：
-      <Button block><Link to="/Home">Home页面</Link></Button>
-      <Button block ><Link to="/Register">Register页面</Link></Button>
-      <Button block><Link to="/Login">Login页面</Link></Button>
-      <Button block><Link to="/Resetpsd">Resetpsd页面</Link></Button>
-      <Button block><Link to="/Forgetpsd">Forgetpsd页面</Link></Button>
-      <Button block><Link to="/Allclub">Allclub页面</Link></Button>
-      <Button block><Link to="/Createclub">Createclub页面</Link></Button>
-      <Button block><Link to="/Personal">Personal页面</Link></Button>
-      <Button block><Link to="/Manage">Manage页面</Link></Button>
-      <Button block><Link to="/ClubActivity">ClubActivity页面</Link></Button>
-      <Button block><Link to="/Test">Test页面</Link></Button>
+    <div className={styles.all}>
+      项目一览表:
+      <div className={styles.index}>
+        <Button block><Link to="/Home">Home页面</Link></Button>
+        <Button block ><Link to="/Register">Register页面</Link></Button>
+        <Button block><Link to="/Login">Login页面</Link></Button>
+        <Button block><Link to="/Resetpsd">Resetpsd页面</Link></Button>
+        <Button block><Link to="/Forgetpsd">Forgetpsd页面</Link></Button>
+        <Button block><Link to="/Allclub">Allclub页面</Link></Button>
+        <Button block><Link to="/Createclub">Createclub页面</Link></Button>
+        <Button block><Link to="/Personal">Personal页面</Link></Button>
+        <Button block><Link to="/Manage">Manage页面</Link></Button>
+        <Button block><Link to="/ClubActivity">ClubActivity页面</Link></Button>
+        <Button block><Link to="/Test">Test页面</Link></Button>
+        
+      </div>
+
 
     </div>
   );
