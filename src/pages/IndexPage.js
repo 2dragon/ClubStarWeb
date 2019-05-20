cf105fe33df794bbbfbb5645a24a72c92dd0bc05
jsl <920024814@qq.com>
2019-05-20 15:03:58 +0800

    实现路由的自动挂载

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -3,26 +3,19 @@ import { connect } from 'dva';
 import { Button } from 'antd';
 import styles from './IndexPage.scss';
 import { Link } from 'dva/router';
+import { RouteConfig } from '../router.js';
 
 function IndexPage() {
   return (
     <div className={styles.all}>
       项目一览表:
       <div className={styles.index}>
-        <Button block><Link to="/Home">Home页面</Link></Button>
-        <Button block ><Link to="/Register">Register页面</Link></Button>
-        <Button block><Link to="/Login">Login页面</Link></Button>
-        <Button block><Link to="/Resetpsd">Resetpsd页面</Link></Button>
-        <Button block><Link to="/Forgetpsd">Forgetpsd页面</Link></Button>
-        <Button block><Link to="/Allclub">Allclub页面</Link></Button>
-        <Button block><Link to="/Createclub">Createclub页面</Link></Button>
-        <Button block><Link to="/Personal">Personal页面</Link></Button>
-        <Button block><Link to="/Manage">Manage页面</Link></Button>
-        <Button block><Link to="/ClubActivity">ClubActivity页面</Link></Button>
-        <Button block><Link to="/Test">Test页面</Link></Button>
-        <Button block><Link to="/Forum">Forum页面</Link></Button>
-        <Button block><Link to="/Clubhonor">Clubhonor页面</Link></Button>
-        <Button block><Link to="/Notices">Notices页面</Link></Button>
+        {
+          RouteConfig.map((i) => (
+            <Button block><Link to={i.path}>{i.name}页面</Link></Button>
+          )
+          )
+        }
       </div>
     </div>
   );
@@ -32,69 +25,3 @@ IndexPage.propTypes = {
 };
 
 export default connect()(IndexPage);
-
-
-// import React from 'react';
-// import { connect } from 'dva';
-// import { Layout } from 'antd';
-
-// // 引入路由需要的组件
-// import { Switch } from 'dva/router';
-// import SubRoutes, { RedirectRoute, NoMatchRoute } from '../utils/SubRoutes';
-
-// // import NavBar from './NavBar';
-// // import Home from './Home';
-// // import About from './About';
-// // import Admin from './Admin';
-// // import Menus from './Menus';
-// // import Login from './User/Login';
-// // import Register from './User/Register';
-// import styles from './IndexPage.scss';
-
-// const { Content } = Layout;
-
-// function IndexPage(props) {
-//   // console.log('IndexPage.js');
-
-//   // console.log(props);
-//   const { routes, app } = props;
-//   // console.log(routes);
-
-//   return (
-//     <Layout className={styles.layout}>
-//       {/* <Header className={styles.header}>
-//         <NavBar {...props} />
-//       </Header> */}
-
-//       <Content className={styles.content}>
-//         {/* 一级路由 */}
-//         <Switch>
-//           {/* <Route path="/home" component={Home} />
-//           <Route path="/menus" component={Menus} />
-//           <Route path="/admin" component={Admin} />
-//           <Route path="/about" component={About} />
-//           <Route path="/login" component={Login} />
-//           <Route path="/register" component={Register} /> */}
-//           {routes.map((route, i) => (
-//             // 调用封装组件
-//             <SubRoutes key={i} {...route} app={app} />
-//           ))}
-//           {/*  
-//               重定向方式：
-//               如果路由配置中没有redirect: true（通过循环渲染重定向）
-//               则默认第一个路由为重定向路由
-//               <Redirect exact from={"/"} to={routes[0].path} /> 
-//              */}
-//           {/* <Redirect to="/home" /> */}
-//           <RedirectRoute exact={true} from={'/'} routes={routes} />
-//           {/* 输入的链接不存在时,跳转到NoMatch组件中 */}
-//           <NoMatchRoute />
-//         </Switch>
-//       </Content>
-//     </Layout>
-//   );
-// }
-
-// IndexPage.propTypes = {};
-
-// export default connect()(IndexPage);
--- src/router.js
+++ src/router.js
@@ -88,47 +88,4 @@ function RouterConfig({ history, app }) {
 }
 
 export default RouterConfig;
-// import React from 'react';
-// import { Router, Route, Switch } from 'dva/router';
-// import IndexPage from './pages/IndexPage';
-// import Register from './pages/Register';
-// import Login from './pages/Login';
-// import Home from './pages/Home';
-// import Forgetpsd from './pages/Forgetpsd';
-// import Resetpsd from './pages/Resetpsd';
-// import Personal from './pages/Personal';
-// import Createclub from './pages/Createclub';
-// import Allclub from './pages/Allclub';
-// import Manage from './pages/Manage';
-// import ClubActivity from './pages/ClubActivity';
-// import Test from './pages/Test';
-// import Forum from './pages/Forum';
-// import Clubhonor from './pages/Clubhonor';
-// import Notices from './pages/Notices';
-
-// function RouterConfig({ history }) {
-//   return (
-//     <Router history={history}>
-//       <Switch>
-//         <Route path="/" exact component={IndexPage} />
-//         <Route path="/Register" exact component={Register} />
-//         <Route path="/Login" exact component={Login} />
-//         <Route path="/Home" exact component={Home} />
-//         <Route path="/Forgetpsd" exact component={Forgetpsd} />
-//         <Route path="/Resetpsd" exact component={Resetpsd} />
-//         <Route path="/Personal" exact component={Personal} />
-//         <Route path="/Createclub" exact component={Createclub} />
-//         <Route path="/Allclub" exact component={Allclub} />
-//         <Route path="/Manage" exact component={Manage} />
-//         <Route path="/ClubActivity" exact component={ClubActivity} />
-//         <Route path="/Test" exact component={Test} />
-//         <Route path="/Forum" exact component={Forum} />
-//         <Route path="/Clubhonor" exact component={Clubhonor} />
-//         <Route path="/Notices" exact component={Notices} />
-//       </Switch>
-//     </Router>
-//   );
-// }
-
-// export default RouterConfig;
-
+export {RouteConfig};
